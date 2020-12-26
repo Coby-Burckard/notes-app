05deabeb5296c93956798138d7296ae68ac5c8e8
Coby-Burckard <51294008+Coby-Burckard@users.noreply.github.com>
2020-12-26 11:04:50 -0600

    reading tasks and users

--- src/index.js
+++ src/index.js
@@ -9,6 +9,8 @@ const port = process.env.port || 3000
 
 app.use(express.json())
 
+
+//Create
 app.post('/users', (req, res) => {
   const user = new User(req.body)
 
@@ -35,6 +37,52 @@ app.post('/tasks', (req, res) => {
     })
 })
 
+
+//Read
+app.get('/users', (req, res) => {
+  User.find()
+    .then(result => res.send(result))
+    .catch(e => res.status(500).send(e))
+})
+
+app.get('/users/:id', (req, res) => {
+  const _id = req.params.id
+
+  User.findById(_id)
+    .then(user => {
+      if (!user) {
+        return res.status(404).send('unable to find user')
+      }
+      res.send(user)
+    })
+    .catch(e => {
+      res.status(500).send('unable to find user')
+    })
+})
+
+
+app.get('/tasks', (req, res) => {
+  Task.find()
+    .then(result => res.send(result))
+    .catch(e => res.status(500).send(e))
+})
+
+app.get('/tasks/:id', (req, res) => {
+  const _id = req.params.id
+
+  Task.findById(_id)
+    .then(user => {
+      if (!user) {
+        return res.status(404).send('unable to find task')
+      }
+      res.send(user)
+    })
+    .catch(e => {
+      res.status(500).send('unable to find task')
+    })
+})
+
+//Start server
 app.listen(port, () => {
   console.log('hello from server on ', port)
 })
