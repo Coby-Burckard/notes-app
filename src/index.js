495eb9612a34d13a8939b462a9c31ca8439c8235
Coby-Burckard <51294008+Coby-Burckard@users.noreply.github.com>
2020-12-28 09:20:55 -0600

    refactoring for async await and adding update

--- src/index.js
+++ src/index.js
@@ -10,76 +10,116 @@ const port = process.env.port || 3000
 app.use(express.json())
 
 
-//Create
-app.post('/users', (req, res) => {
+//Users
+app.post('/users', async (req, res) => {
   const user = new User(req.body)
 
-  user
-    .save()
-    .then(() => {
-      res.status(201).send(user)
-    })
-    .catch(error => {
-      res.status(400).send(error)
-    })
+  try {
+    await user.save()
+    res.status(201).send(user)
+  } catch (e) {
+    res.status(400).send(e)
+  }
 })
 
-app.post('/tasks', (req, res) => {
+
+app.get('/users', async (req, res) => {
+  try {
+    const result = await User.find()
+    res.send(result)
+  } catch (e) {
+    res.status(500).send(e)
+  }
+})
+
+app.get('/users/:id', async (req, res) => {
+  const _id = req.params.id
+
+  try {
+    const user = await User.findById(_id)
+    if (!user) {
+      return res.status(404).send('unable to find user')
+    }
+    res.send(user)
+  } catch (e) {
+    res.status(500).send('unable to find user')
+  }
+})
+
+app.patch('/users/:id', async (req, res) => {
+  //only certain fields can be updated
+  const allowedUpdates = ['name', 'email', 'password', 'age']
+  const updates = Object.keys(req.body)
+  const isValidOperation = updates.every(key => allowedUpdates.includes(key))
+  if (!isValidOperation) {
+    return res.status(400).send({ error: 'invalid updates!' })
+  }
+
+  //updating user
+  try {
+    const user = await User.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true })
+
+    if (!user) {
+      return res.status(404).send()
+    }
+
+    res.send(user)
+  } catch (e) {
+    res.status(400).send(e)
+  }
+})
+
+
+//Tasks
+app.post('/tasks', async (req, res) => {
   const task = new Task(req.body)
 
-  task
-    .save()
-    .then(() => {
-      res.status(201).send(task)
-    })
-    .catch((e) => {
-      res.status(400).send(e)
-    })
+  try {
+    await task.save()
+    res.status(201).send(task)
+  } catch (e) {
+    res.status(400).send(e)
+  }
 })
 
+app.get('/tasks', async (req, res) => {
 
-//Read
-app.get('/users', (req, res) => {
-  User.find()
-    .then(result => res.send(result))
-    .catch(e => res.status(500).send(e))
+  try {
+    const result = await Task.find()
+    res.send(result)
+  } catch (e) {
+    res.status(500).send(e)
+  }
 })
 
-app.get('/users/:id', (req, res) => {
+app.get('/tasks/:id', async (req, res) => {
   const _id = req.params.id
 
-  User.findById(_id)
-    .then(user => {
-      if (!user) {
-        return res.status(404).send('unable to find user')
-      }
-      res.send(user)
-    })
-    .catch(e => {
-      res.status(500).send('unable to find user')
-    })
+  try {
+    const user = await Task.findById(_id)
+    if (!user) {
+      return res.status(404).send('unable to find task')
+    }
+    res.send(user)
+  } catch (e) {
+    res.status(500).send('unable to find task')
+  }
 })
 
+app.patch('/tasks/:id', async (req, res) => {
+  //validating update fields
+  const allowedUpdates = ['complete', 'description']
+  const isValidOperation = Object.keys(req.body).every(update => allowedUpdates.includes(update))
+  if (!isValidOperation) return res.status(400).send({ error: 'invalid field update request' })
 
-app.get('/tasks', (req, res) => {
-  Task.find()
-    .then(result => res.send(result))
-    .catch(e => res.status(500).send(e))
-})
-
-app.get('/tasks/:id', (req, res) => {
-  const _id = req.params.id
-
-  Task.findById(_id)
-    .then(user => {
-      if (!user) {
-        return res.status(404).send('unable to find task')
-      }
-      res.send(user)
-    })
-    .catch(e => {
-      res.status(500).send('unable to find task')
-    })
+  //updating task 
+  try {
+    const task = await Task.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true })
+    if (!task) return res.status(404).send('bad')
+    res.send(task)
+  } catch (e) {
+    res.status(400).send(e)
+  }
 })
 
 //Start server
